56b4f358e449f3bb16f7fb37dac8d6ddd0891d75
Soroush Abbasi Koohpayegani <soroush@umbc.edu>
2021-09-03 00:27:19 -0400

    Update handwrite.js

--- handwrite.js
+++ handwrite.js
@@ -276,7 +276,7 @@ function main() {
 	// const [minSize, maxSize] = gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE);
 	// // gl.lineWidth(1.7);
 	// gl.lineWidth(minSize*line_width + maxSize*(1-line_width));
-	gl.viewport(0 , 0 , 2100 , 500) ;
+	gl.viewport(0 , 0 , 3000 , 1000) ;
   	initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE) ;
 
 
