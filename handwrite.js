6be88653a387fc02ba0f2ab4491b84ffae54042d
Soroush Abbasi Koohpayegani <62820830+soroush-abbasi@users.noreply.github.com>
2024-01-31 23:12:28 -0800

    Update handwrite.js

--- handwrite.js
+++ handwrite.js
@@ -225,6 +225,9 @@ function drawString()
     var data_dict = {} ; 
     for(var k = 0 ; k < keyBufferSize ; k++)
 	    {
+	 	if(key == -33)
+			continue; 
+		    
 		var key = keyBuffer[k] ;  
 		var cp_list = []
 	    	for(var i = 0 ; i < myFont[key]['numOfSplines'] ; i++)
