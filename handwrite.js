1d8ea1ee782a2fb4d00444eb4b76afb15e9ab153
Soroush Abbasi Koohpayegani <62820830+soroush-abbasi@users.noreply.github.com>
2024-01-31 21:54:11 -0800

    Update handwrite.js

--- handwrite.js
+++ handwrite.js
@@ -1,4 +1,3 @@
-import * as fs from "fs";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -174,22 +173,24 @@ function initModelFont()
 		else
 			myFont[i] = mFontParser(txt,1.0 , 1.0) ;
 	}
+	
 
 	const data = JSON.stringify(myFont);
+	console.log(data)
 
-	// writing the JSON string content to a file
-	fs.writeFile("data.json", data, (error) => {
-	// throwing the error
-	// in case of a writing problem
-	if (error) {
-	// logging the error
-	console.error(error);
+	// // writing the JSON string content to a file
+	// fs.writeFile("data.json", data, (error) => {
+	// // throwing the error
+	// // in case of a writing problem
+	// if (error) {
+	// // logging the error
+	// console.error(error);
 	
-	throw error;
-	}
+	// throw error;
+	// }
 	
-	console.log("data.json written correctly");
-	});
+	// console.log("data.json written correctly");
+	// });
 
 }
 
